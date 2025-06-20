3e60180a09ac7eceecb59f2bb8c8b9fdbb459b53
Alex Armstrong <aarmstrong@divergent3d.com>
2025-06-20 16:19:52 -0700

    Updates to windows configuration and install

--- install.ps1
+++ install.ps1
@@ -11,6 +11,13 @@ function New-HardLinkChecked {
         # Directory target provided so assume caller wants to use target filename
         $Path = Join-Path -Path $Path -ChildPath (Split-Path -Path $Target -Leaf)
     }
+    $drive1 = ([System.IO.Path]::GetPathRoot($Path)).ToUpper()
+    $drive2 = ([System.IO.Path]::GetPathRoot($Target)).ToUpper()
+    if ($drive1 -ne $drive2) {
+        Write-Host "WARNING: Paths are on different drives: $drive1 and $drive2"
+    # } else {
+    #     Write-Host "DEBUG: Paths are both on drive $drive1"
+    }
     if (Test-Path $Path) {
         Write-Host "Target path already exists. Consider renaming: $Path"
         return
@@ -20,14 +27,18 @@ function New-HardLinkChecked {
 }
 
 # Powershell dotfiles
-$tgtPath = (Get-Item ".\home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1").FullName
-$dstDir = Split-Path -Path $PROFILE -Parent
+$tgtPath = (Get-Item ".\windows\Microsoft.PowerShell_profile.ps1").FullName
+# NB: $PROFILE may not exist yet
+$dstDir = [System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($PROFILE))
 
-$dstPath = Join-Path -Path $dstDir -ChildPath "Microsoft.PowerShell_profile.ps1"
+$dstPath = Join-Path -Path $dstDir -ChildPath "PowerShell/Microsoft.PowerShell_profile.ps1"
 New-HardLinkChecked -Path $dstPath -Target $tgtPath
-$dstPath = Join-Path -Path $dstDir -ChildPath "Microsoft.VSCode_profile.ps1"
+$dstPath = Join-Path -Path $dstDir -ChildPath "PowerShell/Microsoft.VSCode_profile.ps1"
 New-HardLinkChecked -Path $dstPath -Target $tgtPath
-
+$dstPath = Join-Path -Path $dstDir -ChildPath "WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
+New-HardLinkChecked -Path $dstPath -Target $tgtPath
+$dstPath = Join-Path -Path $dstDir -ChildPath "WindowsPowerShell/Microsoft.VSCode_profile.ps1"
+New-HardLinkChecked -Path $dstPath -Target $tgtPath
 
 # VSCode dotfiles
 $tgtPath = (Get-Item ".\config\vscode\keybindings.json").FullName
--- windows/Microsoft.Powershell_profile.ps1
+++ windows/Microsoft.Powershell_profile.ps1
@@ -1,8 +1,14 @@
 # Probably need to run the following manually first
 # Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 
+# Environment configuration
 $Env:XDG_CONFIG_HOME = (Get-Item "$HOME\dotfiles\config").FullName
 
+# Command line configuration
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key 'Ctrl+u' -Function BackwardKillLine
 Set-PSReadLineKeyHandler -Key 'Ctrl+k' -Function KillLine
+
+function prompt {
+    "[$(Get-Date -Format 'HH:mm:ss')] $(Get-Location)`r`n> "
+}
